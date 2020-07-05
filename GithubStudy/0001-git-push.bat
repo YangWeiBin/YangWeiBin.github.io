0318d88c901ec86d397c20044b635d45ec40acb6
YangWeiBin <863255386@qq.com>
2020-07-05 16:32:48 +0800

    20200705163248

--- GithubStudy/0001-git-push.bat
+++ GithubStudy/0001-git-push.bat
@@ -2,3 +2,4 @@ set THISDATETIME=%DATE:~0,4%%DATE:~5,2%%DATE:~8,2%%TIME:~0,2%%TIME:~3,2%%TIME:~6
 git add .
 git commit -m %THISDATETIME%
 git push
+pause
--- GithubStudy/git-rename.bat
+++
@@ -1,2 +0,0 @@
-set path=%cd%
-start F:\GithubRepository\RenameFile\RenameForGithub.exe %path%
